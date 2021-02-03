92a62a6b2bc74164a84ef4e7d73f446ad4e0fd3e
mmyeon <mindfulyeon@gmail.com>
2021-02-03 16:14:09 +0900

    [setting] GlobalStyled 적용

---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset};
+
+    body { 
+        font-family: "Poppins", sans-serif;
+    }
+
+`;
+
+export default GlobalStyles;
