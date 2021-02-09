c8a114c442213b1cbc359a61d124889659f540e9
mmyeon <mindfulyeon@gmail.com>
2021-02-09 16:19:16 +0900

    [Home 컴포넌트] 첫 시작화면 제작

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button";
+
+const Home = () => {
+  return (
+    <>
+      <div>Dog In</div>
+      <h1>나는 너와 함께 살 준비가 되어있을까?</h1>
+      <Button />
+    </>
+  );
+};
+
+export default Home;
