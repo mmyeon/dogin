796f7671d3ec1cd8bd5f2dbbd488ab05d77232b4
mmyeon <mindfulyeon@gmail.com>
2021-02-09 23:19:33 +0900

    [draft] 버튼 클릭시 Detail컴포넌트 보여주기

--- src/Routes/Question1.js
+++ src/Routes/Question1.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../Components/Button";
 
 const Question1 = () => {
-  return <div>Question1</div>;
+  const [userResult, setUserResult] = useState([]);
+
+  function handleYesClick() {
+    console.log("클릭되었습니다");
+  }
+
+  return (
+    <>
+      <div>Question1</div>
+      {/* TODO: 클릭하면 userResult에 추가되기 */}
+      <Button title="YES" onClick={handleYesClick} />
+      <Button title="NO" onClick={handleYesClick} />
+    </>
+  );
 };
 
 export default Question1;
