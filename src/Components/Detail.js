dc3edde8035aa4d32fc1973b26699ae6674bb437
mmyeon <mindfulyeon@gmail.com>
2021-02-16 10:12:58 +0900

    [Fix] 스타일드 컴포넌트 함수 외부로 분리

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -2,36 +2,37 @@ import React from "react";
 import styled from "styled-components";
 import Button from "./Button";
 
+const ModalOverlay = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: black;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const ModalContainer = styled.div`
+  width: 400px;
+  height: 400px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: red;
+`;
+
+const Title = styled.h1`
+  color: white;
+`;
+
 const Detail = ({ setQuestionNum, questionNum }) => {
-  const ModalOverlay = styled.div`
-    height: 100vh;
-    width: 100vw;
-    background: black;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 0;
-    left: 0;
-  `;
-
-  const ModalContainer = styled.div`
-    width: 400px;
-    height: 400px;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: red;
-  `;
-
-  const Title = styled.h1`
-    color: white;
-  `;
-
   function updateQuestion() {
     setQuestionNum(questionNum + 1);
+    console.log(questionNum);
   }
 
   return (
