cc717b70cc7fe9e1e4068d4ad125717d9a56f3ae
mmyeon <mindfulyeon@gmail.com>
2021-02-19 14:54:39 +0900

    [data] 질문과 상세설명 데이터 만들기

---
+++ src/data.js
@@ -0,0 +1,13 @@
+const datas = {
+  1: {
+    question: "매일 한 시간 이상 산책할 수 있나요?",
+    detail: "1번 상세 설명",
+  },
+  2: {
+    question:
+      "개는 자주 아플 수 있습니다. 1년에 백만원 이상의 병원비를 감당할 수 있습니까?",
+    detail: "2번 상세 설명",
+  },
+};
+
+export default datas;
