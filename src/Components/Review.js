f409dc72d0f02b4cd4ccb369ec183633bb3394d3
mmyeon <mindfulyeon@gmail.com>
2021-03-03 16:58:44 +0900

    [Review 컴포넌트] 유저 선택값과 정답값 비교해서 보여주기

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,27 +1,44 @@
 import React from "react";
 import { useUserAnswerList } from "../context";
+import data from "../data";
 
 const Review = () => {
   const userAnswerList = useUserAnswerList();
+  const correctAnswerList = data.map(
+    (item, i) => item[i + 1]["questionAnswer"]
+  );
+
+  let resultList = [];
+  function checkResult() {
+    for (let i = 0; i < correctAnswerList.length; i++) {
+      userAnswerList[i]["userAnswer"] === correctAnswerList[i]
+        ? resultList.push({ questionNumber: i + 1, answerResult: "맞음" })
+        : resultList.push({ questionNumber: i + 1, answerResult: "틀림" });
+    }
+  }
+
+  checkResult();
+
+  const correctNumber = resultList.filter(
+    (answer) => answer.answerResult === "맞음"
+  ).length;
+
+  const incorrectNumber = resultList.filter(
+    (answer) => answer.answerResult === "틀림"
+  ).length;
 
   return (
     <section>
-      <h1>
-        Yes :{" "}
-        {userAnswerList.filter((answer) => answer.userAnswer === "YES").length}
-      </h1>
+      <h1>맞음 : {correctNumber}</h1>
       <h2>
-        {userAnswerList
-          .filter((answer) => answer.userAnswer === "YES")
+        {resultList
+          .filter((answer) => answer.answerResult === "맞음")
           .map((item) => `${item.questionNumber} 번 문제, `)}
       </h2>
-      <h1>
-        No :{" "}
-        {userAnswerList.filter((answer) => answer.userAnswer === "NO").length}{" "}
-      </h1>
+      <h1>틀림 : {incorrectNumber} </h1>
       <h2>
-        {userAnswerList
-          .filter((answer) => answer.userAnswer === "NO")
+        {resultList
+          .filter((answer) => answer.answerResult === "틀림")
           .map((item) => `${item.questionNumber} 번 문제, `)}
       </h2>
     </section>
--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ const data = [
     1: {
       question: "매일 한 시간 이상 산책할 수 있나요?",
       answer: "1번 상세 설명",
+      questionAnswer: "YES",
     },
   },
   {
@@ -10,36 +11,42 @@ const data = [
       question:
         "개는 자주 아플 수 있습니다. 1년에 백만원 이상의 병원비를 감당할 수 있습니까?",
       answer: "2번 상세 설명",
+      questionAnswer: "YES",
     },
   },
   {
     3: {
       question: "개가 먹으면 안되는 음식은?",
       answer: "3번 상세 설명",
+      questionAnswer: "YES",
     },
   },
   {
     4: {
       question: "행동훈련?",
       answer: "4번 상세 설명",
+      questionAnswer: "YES",
     },
   },
   {
     5: {
       question: "5번 문제?",
       answer: "5번 상세 설명",
+      questionAnswer: "YES",
     },
   },
   {
     6: {
       question: "6번 문제?",
       answer: "6번 상세 설명",
+      questionAnswer: "YES",
     },
   },
   {
     7: {
       question: "7번 문제?",
       answer: "7번 상세 설명",
+      questionAnswer: "YES",
     },
   },
 ];
