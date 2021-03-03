2e35e1cfd71b90627ca402ce64013bfe1d29a48c
mmyeon <mindfulyeon@gmail.com>
2021-03-03 14:42:48 +0900

    [Review 컴포넌트] 유저의 선택 전체결과 보여주기

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useUserAnswerList } from "../context";
 
 const Review = () => {
-  return <div>리뷰입니다</div>;
+  const userAnswerList = useUserAnswerList();
+
+  return (
+    <section>
+      <h1>
+        Yes :{" "}
+        {userAnswerList.filter((answer) => answer.userAnswer === "YES").length}
+      </h1>
+      <h2>
+        {userAnswerList
+          .filter((answer) => answer.userAnswer === "YES")
+          .map((item) => `${item.questionNumber} 번 문제, `)}
+      </h2>
+      <h1>
+        No :{" "}
+        {userAnswerList.filter((answer) => answer.userAnswer === "NO").length}{" "}
+      </h1>
+      <h2>
+        {userAnswerList
+          .filter((answer) => answer.userAnswer === "NO")
+          .map((item) => `${item.questionNumber} 번 문제, `)}
+      </h2>
+    </section>
+  );
 };
 
 export default Review;
