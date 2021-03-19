2c5861f126decb9ef93a5999dd700348a108a376
mmyeon <mindfulyeon@gmail.com>
2021-03-19 18:12:14 +0900

    [Refactoring] Answer 컴포넌트 불필요한 styled-component 통합

--- src/Components/Answer.js
+++ src/Components/Answer.js
@@ -33,21 +33,37 @@ const Title = styled.span`
     1px 1px 0 #000;
 `;
 
+const BackgroundCircle = styled.div`
+  display: flex;
+  .circle {
+    width: 45px;
+    height: 45px;
+    background: ${(props) => props.backgroundColor};
+    border: 1px solid black;
+    border-radius: 50%;
+    position: relative;
+
+    &.left {
+      left: 0px;
+    }
+
+    &.right {
+      right: 0px;
+    }
+  }
+`;
+
 const Answer = ({ title }) => {
   return (
     <>
       <AnswerContainer>
-        <CircleContainer>
-          {/* TODO:최소한의 정보만 받아야 한다.  */}
-          <Circle
-            left={"8px"}
-            backgroundColor={title === "Great" ? "#219721" : "#FA5656"}
-          />
-          <Circle
-            right={"8px"}
-            backgroundColor={title === "Great" ? "#219721" : "#FA5656"}
-          />
-        </CircleContainer>
+        <BackgroundCircle
+          backgroundColor={title === "Great" ? "#219721" : "#FA5656"}
+        >
+          <div className="circle left" />
+          <div className="circle right" />
+        </BackgroundCircle>
+
         <Title>{title}</Title>
       </AnswerContainer>
     </>
