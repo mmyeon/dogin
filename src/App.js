61fac6f269d5159b538ec6f6734646f89ea21577
mmyeon <mindfulyeon@gmail.com>
2021-04-08 14:12:28 +0900

    [Fix] og 오류 해결

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import ContentProvider from "./context";
 import GlobalStyles from "../src/GlobalStyles";
 import Router from "./Router";
 import data from "./data";
+import { HelmetProvider } from "react-helmet-async";
+
+const helmetContext = {};
 
 const App = () => {
   return (
     <ContentProvider data={data}>
-      <Router />
-      <GlobalStyles />
+      <HelmetProvider context={helmetContext}>
+        <Router />
+        <GlobalStyles />
+      </HelmetProvider>
     </ContentProvider>
   );
 };
--- src/SEO.js
+++ src/SEO.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Helmet } from "react-helmet-async";
 
 const SEO = ({ meta }) => {
   return (
-    <HelmetProvider>
-      <Helmet>
-        {meta.map((item, i) => (
-          <meta
-            key={i.toString()}
-            property={item.property}
-            content={item.content}
-          ></meta>
-        ))}
-      </Helmet>
-    </HelmetProvider>
+    <Helmet>
+      {meta.map((item, i) => (
+        <meta
+          key={i.toString()}
+          property={item.property}
+          content={item.content}
+        ></meta>
+      ))}
+    </Helmet>
   );
 };
 
