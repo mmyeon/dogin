ff8b9cfc98c7bd970c7e3634e2e0e54c51c55a81
mmyeon <mindfulyeon@gmail.com>
2021-04-12 13:16:08 +0900

    [Fix] Button 컴포넌트 > 모바일, 태블릿 컬러 변경

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-import { device } from "../breakpoints";
+import { device, size } from "../breakpoints";
 
 const ButtonContainer = styled.button`
   font-family: "Poppins", sans-serif;
-  background: white;
   color: #c8c5d2;
   font-size: 1.1rem;
   padding: 0.4em 1.5em;
-  border: 2px solid #c8c5d2;
   border-radius: 30px;
   cursor: pointer;
   outline: none;
   font-weight: 900;
   letter-spacing: 3px;
+  border: 2px solid black;
   margin-right: ${(props) => props.marginRight || "0px"};
+  background: mediumaquamarine;
+  color: white;
 
-  @media ${device.tablet} {
+  @media ${device.laptop} {
+    background: white;
+    border-color: #c8c5d2;
     padding: 0.5em 2em;
+    color: #c8c5d2;
   }
 
   &:hover {
