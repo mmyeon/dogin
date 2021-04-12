333a45e054b66c592b9729caecde56a091a90027
mmyeon <mindfulyeon@gmail.com>
2021-04-13 02:40:46 +0900

    [Fix] 이전 스크롤 유지되는 문제 해결

--- src/routes/Quiz/index.js
+++ src/routes/Quiz/index.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Question from "./Question";
 import Answer from "./Answer";
+import ScrollToTop from "../../components/ScrollToTop";
 
 const Quiz = () => {
   const [hasResult, setHasResult] = useState(false);
 
   return (
     <div>
+      <ScrollToTop />
       {!hasResult && <Question setAsAnswered={setAsAnswered} />}
       {hasResult && <Answer />}
     </div>
