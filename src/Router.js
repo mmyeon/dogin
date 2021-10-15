c8e612486ce3b7b80f71561e28b8461f56f7c3af
mmyeon <mindfulyeon@gmail.com>
2021-10-15 18:01:21 +0900

    [refactor] 다이나믹 라우팅 코드 개선

--- src/Router.js
+++ src/Router.js
@@ -2,23 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Quiz from "./routes/Quiz";
-import data from "./data";
 import Result from "./routes/Result";
 
 const DefaultRouter = () => {
   return (
     <Router>
       <Route path="/" exact component={Home} />
-      {data.map((question, i) => {
-        return (
-          <Route
-            key={i.toString()}
-            path={`/quiz/${i + 1}`}
-            exact
-            component={Quiz}
-          />
-        );
-      })}
+      <Route path="/quiz/:quizNumber" exact component={Quiz} />
       <Route path="/result" exact component={Result} />
     </Router>
   );
