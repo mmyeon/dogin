b4f861a87f12746387d81b0de4b697f2337e45f3
mmyeon <mindfulyeon@gmail.com>
2021-11-12 17:32:07 +0900

    [style] Modal 컴포넌트 스타일 추가

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,34 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 
-const ModalOverlay = styled.div`
-  background: yellow;
+const ModalContainer = styled.div`
+  width: 100%;
+  max-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ModalShadow = styled.div`
+  background: black;
+  opacity: 0.7;
+  width: 100%;
+  height: 100%;
   top: 0;
-  right: 0;
-  bottom: 0;
   left: 0;
-  z-index: 1000;
   position: fixed;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-const ModalContent = styled.div`
+const Modal = styled.div`
   background: white;
-  width: 50%;
-  height: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 1.3em;
+  border-radius: 10px;
 `;
 
-const Modal = ({ setIsOpen }) => {
+const ModalContent = styled.div``;
+
+const StyledModal = ({ setIsOpen }) => {
   return (
-    <ModalOverlay onClick={() => setIsOpen(false)}>
-      <ModalContent>can you see?</ModalContent>
-    </ModalOverlay>
+    <ModalContainer>
+      <ModalShadow onClick={() => setIsOpen(false)} />
+      <Modal>
+        <ModalContent></ModalContent>
+      </Modal>
+    </ModalContainer>
   );
 };
 
-export default Modal;
+export default StyledModal;
