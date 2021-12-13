143c73954d16703f7d527d92f61e9e0b48b39afa
mmyeon <mindfulyeon@gmail.com>
2021-12-13 23:49:14 +0900

    [feat] 카카오 SDK 초기화

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GlobalStyles from "./styles/GlobalStyles";
 import Router from "./Router";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 const App = () => {
+  const { Kakao } = window;
+  const KAKAO_KEY = "19da7d5b72a371eb93bc0ab13606c4e7";
+
+  useEffect(() => {
+    Kakao.init(KAKAO_KEY);
+  }, []);
+
   return (
     <Provider store={store}>
       <Router />
