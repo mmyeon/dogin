87d4c21c9681961f1edf2a1711ace65924957c86
mmyeon <mindfulyeon@gmail.com>
2021-12-15 18:23:48 +0900

    [feat] 링크 복사 버튼, 카카오톡 버튼 추가

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 
 const ModalContainer = styled.div`
@@ -28,21 +28,132 @@ const Modal = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  padding: 1.3em;
+  padding: 2.5em;
   border-radius: 10px;
+`;
+
+const ModalTitle = styled.h1`
+  font-size: 1.5em;
+  margin-bottom: 1em;
+  letter-spacing: 1px;
+  word-break: keep-all;
+  white-space: break-spaces;
+  line-height: 1.1em;
 `;
 
 const ModalContent = styled.div``;
 
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+
+  > .home-url {
+    position: absolute;
+    width: 20px;
+    top: -9000px;
+  }
+
+  > .button {
+    cursor: pointer;
+  }
+
+  > .button + .button {
+    margin-left: 1.5em;
+  }
+`;
+
+const KakaoButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  width: 68px;
+  height: 68px;
+  background: #fee500;
+  border-radius: 50%;
+`;
+
+const LinkButton = styled.button`
+  width: 68px;
+  height: 68px;
+  background: mediumaquamarine;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  color: black;
+`;
+
 const StyledModal = ({ setIsOpen }) => {
+  const { Kakao } = window;
+  const urlInput = useRef(null);
+
   return (
     <ModalContainer>
       <ModalShadow onClick={() => setIsOpen(false)} />
       <Modal>
-        <ModalContent></ModalContent>
+        <ModalContent>
+          <ModalTitle>친구에게 공유해 보세요 😊 </ModalTitle>
+          <ButtonContainer>
+            <KakaoButton className="button">
+              <img
+                onClick={shareToKakao}
+                src="/assets/kakaotalk.svg"
+                alt="kakao share"
+              />
+            </KakaoButton>
+
+            <LinkButton onClick={copyToClipboard} className="button">
+              <i className="fas fa-link fa-2x"></i>
+            </LinkButton>
+
+            <input
+              type="text"
+              className="home-url"
+              value="https://dogin.mmyeon.com"
+              readOnly
+              ref={urlInput}
+            />
+          </ButtonContainer>
+        </ModalContent>
       </Modal>
     </ModalContainer>
   );
+
+  function shareToKakao() {
+    Kakao.Link.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "반려견 입양을 앞두고 계신가요?",
+        description: "좋은 보호자가 될 수 있을지 체크해보세요.",
+        imageUrl: "https://ifh.cc/g/ChgM59.jpg",
+        link: {
+          mobileWebUrl: "https://dogin.mmyeon.com",
+          webUrl: "https://dogin.mmyeon.com",
+        },
+      },
+      buttons: [
+        {
+          title: "나도 테스트 해보기",
+          link: {
+            mobileWebUrl: "https://dogin.mmyeon.com",
+            webUrl: "https://dogin.mmyeon.com",
+          },
+        },
+      ],
+    });
+  }
+
+  function copyToClipboard() {
+    const homeUrlInput = urlInput.current;
+    homeUrlInput.select();
+    document.execCommand("copy");
+    window.alert("주소가 복사되었습니다.😊");
+    setIsOpen(false);
+  }
 };
 
 export default StyledModal;
