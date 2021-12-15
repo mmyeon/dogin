814a34772276d15a518b5918701940ee6888a8db
mmyeon <mindfulyeon@gmail.com>
2021-12-15 14:00:33 +0900

    [refactor] 카카오 API키 환경변수에 숨기기

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import store from "./redux/store";
 
 const App = () => {
   const { Kakao } = window;
-  const KAKAO_KEY = "19da7d5b72a371eb93bc0ab13606c4e7";
+  const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY;
 
   useEffect(() => {
     Kakao.init(KAKAO_KEY);
